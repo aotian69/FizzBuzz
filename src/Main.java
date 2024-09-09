68a4700fa58607f1687517d2b87189b64669dd8c
aotian69 <ao.tian@mail.utoronto.ca>
2024-09-09 12:17:05 -0400

    Main Java Class Added

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+// A main method which will attempt to call FizzBuzz.doFizzBuzz
+class Main{
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.main();
+    }
+}
